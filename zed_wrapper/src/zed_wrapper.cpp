56ad93d86fd543205ca7f8e62608c5b7c5c76a19
Walter Lucetti <walter.lucetti@stereolabs.com>
2020-09-14 12:20:49 +0200

    Add zed_wrapper to debug zed_components using manual composition

---
+++ zed_wrapper/src/zed_wrapper.cpp
@@ -0,0 +1,29 @@
+
+#include <rclcpp/rclcpp.hpp>
+#include "zed_components/zed_camera_component.hpp"
+
+int main(int argc, char * argv[])
+{
+    // Force flush of the stdout buffer.
+    setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+
+    // Initialize any global resources needed by the middleware and the client library.
+    // This will also parse command line arguments one day (as of Beta 1 they are not used).
+    // You must call this before using any other part of the ROS system.
+    // This should be called once per process.
+    rclcpp::init(argc, argv);
+
+    // Create an executor that will be responsible for execution of callbacks for a set of nodes.
+    // With this version, all callbacks will be called from within this thread (the main one).
+    rclcpp::executors::SingleThreadedExecutor exec;
+    rclcpp::NodeOptions options;
+
+    // spin will block until work comes in, execute work as it becomes available, and keep blocking.
+    // It will only be interrupted by Ctrl-C.
+    exec.spin();
+
+    rclcpp::shutdown();
+
+    return 0;
+}
+

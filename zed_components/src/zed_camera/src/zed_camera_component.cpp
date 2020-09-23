70f7152066b7a88f60e8e35564e89d35fa3a054c
Walter Lucetti <walter.lucetti@stereolabs.com>
2020-09-23 16:14:12 +0200

    Dynamic Gamma

--- zed_components/src/zed_camera/src/zed_camera_component.cpp
+++ zed_components/src/zed_camera/src/zed_camera_component.cpp
@@ -705,7 +705,7 @@ rcl_interfaces::msg::SetParametersResult ZedCamera::callback_paramChange(std::ve
             result.successful = true;
             result.reason = param.get_name() + " correctly set.";
             return result;
-        }else if(param.get_name() == "video.sharpness" ) {
+        } else if(param.get_name() == "video.sharpness" ) {
 
             rclcpp::ParameterType correctType = rclcpp::ParameterType::PARAMETER_INTEGER;
             if( param.get_type() != correctType ) {
@@ -723,6 +723,29 @@ rcl_interfaces::msg::SetParametersResult ZedCamera::callback_paramChange(std::ve
             }
 
             mCamSharpness = val;
+
+            RCLCPP_INFO_STREAM(get_logger(), "Parameter '" << param.get_name() << "' correctly set to " << val);
+            result.successful = true;
+            result.reason = param.get_name() + " correctly set.";
+            return result;
+        } else if(param.get_name() == "video.gamma" ) {
+
+            rclcpp::ParameterType correctType = rclcpp::ParameterType::PARAMETER_INTEGER;
+            if( param.get_type() != correctType ) {
+                result.successful = false;
+                result.reason = param.get_name() + " must be a " + rclcpp::to_string(correctType);
+                return result;
+            }
+
+            int val = param.as_int();
+
+            if( (val < 0) || (val > 8) ) {
+                result.successful = false;
+                result.reason = param.get_name() + " must be a positive integer in the range [0,8]";
+                return result;
+            }
+
+            mCamGamma = val;
 
             RCLCPP_INFO_STREAM(get_logger(), "Parameter '" << param.get_name() << "' correctly set to " << val);
             result.successful = true;

fa526f30f854d577548ddb06052cf60c0a4eebfd
Walter Lucetti <walter.lucetti@stereolabs.com>
2021-06-01 15:51:16 +0200

    Add support for ZED SDK v3.5 and new ZED2i

---
+++ zed_wrapper/launch/zed2i.launch.py
@@ -0,0 +1,71 @@
+import os
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    # Define LaunchDescription variable
+    ld = LaunchDescription()
+
+    # use:
+    #  - 'zed' for "ZED" camera
+    #  - 'zedm' for "ZED mini" camera
+    #  - 'zed2' for "ZED2" camera
+    #  - 'zed2i' for "ZED2i" camera
+    camera_model = 'zed2i'
+
+    # Camera name
+    camera_name = 'zed2i'
+
+    # URDF file to be loaded by Robot State Publisher
+    urdf = os.path.join(
+        get_package_share_directory('zed_wrapper'),
+        'urdf', camera_model + '.urdf'
+    )
+
+    # ZED Configurations to be loaded by ZED Node
+    config_common = os.path.join(
+        get_package_share_directory('zed_wrapper'),
+        'config',
+        'common.yaml'
+    )
+
+    config_camera = os.path.join(
+        get_package_share_directory('zed_wrapper'),
+        'config',
+        camera_model + '.yaml'
+    )
+
+    # Set LOG format
+    os.environ['RCUTILS_CONSOLE_OUTPUT_FORMAT'] = '{time} [{name}] [{severity}] {message}'
+
+    # Robot State Publisher node
+    rsp_node = Node(
+        package='robot_state_publisher',
+        namespace="/"+camera_name,
+        executable='robot_state_publisher',
+        name=camera_name+'_state_publisher',
+        output='screen',
+        arguments=[urdf],
+    )
+
+    # ZED Wrapper node
+    zed_wrapper_node = Node(
+        package='zed_wrapper',
+        namespace="/"+camera_name,
+        executable='zed_wrapper',
+        name='zed_node',
+        output='screen',
+        parameters=[
+            config_common,  # Common parameters
+            config_camera,  # Camera related parameters
+        ]
+    )
+
+    # Add nodes to LaunchDescription
+    ld.add_action(rsp_node)
+    ld.add_action(zed_wrapper_node)
+
+    return ld

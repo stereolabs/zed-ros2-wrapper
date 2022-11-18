503010310fe6391ba70401d907db45a754362228
Walter Lucetti <walter.lucetti@stereolabs.com>
2022-11-18 20:02:01 +0100

    [WIP] Code formatting

---
+++ zed_components/src/include/visibility_control.hpp
@@ -0,0 +1,57 @@
+// Copyright 2022 Stereolabs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef VISIBILITY_CONTROL_HPP_
+#define VISIBILITY_CONTROL_HPP_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// This logic was borrowed (then namespaced) from the examples on the gcc wiki:
+//     https://gcc.gnu.org/wiki/Visibility
+
+#if defined _WIN32 || defined __CYGWIN__
+#ifdef __GNUC__
+#define ZED_COMPONENTS_EXPORT __attribute__((dllexport))
+#define ZED_COMPONENTS_IMPORT __attribute__((dllimport))
+#else
+#define ZED_COMPONENTS_EXPORT __declspec(dllexport)
+#define ZED_COMPONENTS_IMPORT __declspec(dllimport)
+#endif
+#ifdef ZED_COMPONENTS_BUILDING_DLL
+#define ZED_COMPONENTS_PUBLIC ZED_COMPONENTS_EXPORT
+#else
+#define ZED_COMPONENTS_PUBLIC ZED_COMPONENTS_IMPORT
+#endif
+#define ZED_COMPONENTS_PUBLIC_TYPE ZED_COMPONENTS_PUBLIC
+#define ZED_COMPONENTS_LOCAL
+#else
+#define ZED_COMPONENTS_EXPORT __attribute__((visibility("default")))
+#define ZED_COMPONENTS_IMPORT
+#if __GNUC__ >= 4
+#define ZED_COMPONENTS_PUBLIC __attribute__((visibility("default")))
+#define ZED_COMPONENTS_LOCAL __attribute__((visibility("hidden")))
+#else
+#define ZED_COMPONENTS_PUBLIC
+#define ZED_COMPONENTS_LOCAL
+#endif
+#define ZED_COMPONENTS_PUBLIC_TYPE
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // VISIBILITY_CONTROL_HPP_
--- zed_wrapper/src/zed_wrapper.cpp
+++ zed_wrapper/src/zed_wrapper.cpp
@@ -1,58 +1,48 @@
-/******************************************************************************** 
- * MIT License
- * 
- * Copyright (c) 2020 Stereolabs
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- ********************************************************************************/
+// Copyright 2022 Stereolabs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include <rclcpp/rclcpp.hpp>
+
 #include "zed_components/zed_camera_component.hpp"
 
 int main(int argc, char * argv[])
 {
-    // Force flush of the stdout buffer.
-    setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+  // Force flush of the stdout buffer.
+  setvbuf(stdout, NULL, _IONBF, BUFSIZ);
 
-    // Initialize any global resources needed by the middleware and the client library.
-    // This will also parse command line arguments one day (as of Beta 1 they are not used).
-    // You must call this before using any other part of the ROS system.
-    // This should be called once per process.
-    rclcpp::init(argc, argv);
+  // Initialize any global resources needed by the middleware and the client library.
+  // This will also parse command line arguments one day (as of Beta 1 they are not used).
+  // You must call this before using any other part of the ROS system.
+  // This should be called once per process.
+  rclcpp::init(argc, argv);
 
-    // Create an executor that will be responsible for execution of callbacks for a set of nodes.
-    // With this version, all callbacks will be called from within this thread (the main one).
-    rclcpp::executors::MultiThreadedExecutor exec;
-    rclcpp::NodeOptions options;
+  // Create an executor that will be responsible for execution of callbacks for a set of nodes.
+  // With this version, all callbacks will be called from within this thread (the main one).
+  rclcpp::executors::MultiThreadedExecutor exec;
+  rclcpp::NodeOptions options;
 
-    options.use_intra_process_comms(true);
+  options.use_intra_process_comms(true);
 
-    // Add zed_camera node
-    auto zed_node = std::make_shared<stereolabs::ZedCamera>(options);
-    exec.add_node(zed_node);
+  // Add zed_camera node
+  auto zed_node = std::make_shared<stereolabs::ZedCamera>(options);
+  exec.add_node(zed_node);
 
-    // spin will block until work comes in, execute work as it becomes available, and keep blocking.
-    // It will only be interrupted by Ctrl-C.
-    exec.spin();
+  // spin will block until work comes in, execute work as it becomes available, and keep blocking.
+  // It will only be interrupted by Ctrl-C.
+  exec.spin();
 
-    rclcpp::shutdown();
+  rclcpp::shutdown();
 
-    return 0;
+  return 0;
 }
-

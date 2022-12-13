3130dcfe52d0a0f882db06aca6b3cb9d57a11070
Walter Lucetti <walter.lucetti@stereolabs.com>
2022-12-13 14:11:15 +0000

    Update build_humble_bin.sh

--- .ci/build_humble_bin.sh
+++ .ci/build_humble_bin.sh
@@ -8,23 +8,23 @@ echo "${ttk} Install ROS2 Humble"
 
 echo "${ttk} Set Locale"
 locale  # check for UTF-8
-sudo apt update && sudo apt install -Y locales
+sudo apt update && sudo apt install -y locales
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 locale  # verify settings
 
 echo "${ttk} Setup Sources"
-sudo apt install -Y software-properties-common
+sudo apt install -y software-properties-common
 sudo add-apt-repository universe
-sudo apt update && sudo apt install -Y curl
+sudo apt update && sudo apt install -y curl
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 echo "${ttk} Install ROS 2 packages"
 sudo apt update
 sudo apt upgrade
-sudo apt install -Y ros-humble-ros-base ros-dev-tools
+sudo apt install -y ros-humble-ros-base ros-dev-tools
 
 echo "${ttk} Sourcing the setup script"
 source /opt/ros/humble/setup.bash

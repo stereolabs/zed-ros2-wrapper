077ba5e62c99697243c991220f5652b6e909e5be
Walter Lucetti <walter.lucetti@stereolabs.com>
2022-12-13 17:31:04 +0000

    Update build_humble_bin.sh

--- .ci/build_humble_bin.sh
+++ .ci/build_humble_bin.sh
@@ -34,29 +34,28 @@ echo "${ttk} Install ROS2 Humble"
 
 echo "${ttk} Set Locale"
 locale  # check for UTF-8
-sudo apt-get update && sudo apt-get install -y locales
-sudo locale-gen en_US en_US.UTF-8
-sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+apt-get update && apt-get install -y locales
+locale-gen en_US en_US.UTF-8
+update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 locale  # verify settings
 
 echo "${ttk} Setup Sources"
-sudo apt-get install -y software-properties-common
-sudo add-apt-repository universe
-sudo apt-get update && sudo apt-get install -y curl
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+apt-get install -y software-properties-common
+add-apt-repository universe
+apt-get update && apt-get install -y curl
+curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 echo "${ttk} Install ROS 2 packages"
-sudo apt-get update
-sudo apt-get upgrade --allow-change-held-packages
-sudo apt-get install -y ros-humble-ros-base ros-dev-tools
+apt-get update && apt-get upgrade -y && apt-get autoremove && apt-get autoclean
+apt-get install -y ros-humble-ros-base ros-dev-tools
 
 echo "${ttk} Sourcing the setup script"
 source /opt/ros/humble/setup.bash
 
 echo "${ttk} Initialize rosdep"
-sudo rosdep init
+rosdep init
 rosdep update
 
 echo "${ttk} Check environment variables"

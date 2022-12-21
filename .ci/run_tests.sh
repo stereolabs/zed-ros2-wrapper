232a4de57efc9a61612bb6197ac390f1b04f61a9
Walter Lucetti <walter.lucetti@stereolabs.com>
2022-12-21 17:50:07 +0000

    Update run_tests.sh

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -2,15 +2,22 @@
 set -e
 
 ttk='--->'
+WORKDIR=$(pwd)
 
 echo "${ttk} Check artifact presence"
 ls ros2_ws/install
 
 echo "${ttk} Initialize local ROS2 environment"
+cd ros2_ws/install
+export COLCON_PREFIX_PATH=$(pwd)
 source ros2_ws/install/local_setup.sh
+cd ${WORKDIR}
 
 echo "${ttk} Check ROS2 installation"
 ros2 doctor -r
+
+echo "${ttk} Check ZED ROS2 packages presence"
+ros2 pkg list | grep zed
 
 echo "${ttk} USB peripherals"
 lsusb

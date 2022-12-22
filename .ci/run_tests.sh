95a3b4c7356b6e60d40c5f82ab12e6fcefb96eb2
Walter Lucetti <walter.lucetti@stereolabs.com>
2022-12-22 15:15:03 +0000

    Update run_tests.sh

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -6,15 +6,21 @@ WORKDIR=$(pwd)
 
 
 echo "${ttk} WORKDIR (${WORKDIR})content"
-ls ${WORKDIR}
+ls -lah ${WORKDIR}
 
 echo "${ttk} Check artifact presence"
-ls ../ros2_ws/install
+ls -lah ../ros2_ws/install
+
+ls -lah /builds/sl/ros2_ws/install/zed_interfaces/share/zed_interfaces/
+ls -lah /builds/sl/ros2_ws/install/zed_components/share/zed_components/
+ls -lah /builds/sl/ros2_ws/install/zed_wrapper/share/zed_wrapper/
+ls -lah /builds/sl/ros2_ws/install/zed_ros2/share/zed_ros2/
 
 echo "${ttk} Initialize local ROS2 environment"
 cd ${WORKDIR}
 source ../ros2_ws/install/local_setup.bash
 env | grep COLCON
+env | grep ROS
 
 echo "${ttk} Check ROS2 installation"
 ros2 doctor -r

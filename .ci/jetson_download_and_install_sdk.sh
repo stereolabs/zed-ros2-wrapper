9b1523e079cb4f550299c3688d23222de9f53dda
Walter Lucetti <walter.lucetti@stereolabs.com>
2023-01-13 18:29:05 +0000

    Update jetson_download_and_install_sdk.sh

--- .ci/jetson_download_and_install_sdk.sh
+++ .ci/jetson_download_and_install_sdk.sh
@@ -10,18 +10,18 @@ ZED_SDK_MINOR=$6
 
 ttk="***>"
 
-#sudo echo "Europe/Paris" > /etc/timezone
-#sudo echo "# R${L4T_MAJOR} (release), REVISION: ${L4T_MINOR}" > /etc/nv_tegra_release
+echo "Europe/Paris" > /etc/timezone
+echo "# R${L4T_MAJOR} (release), REVISION: ${L4T_MINOR}" > /etc/nv_tegra_release
     
 #Install ZED SDK
 echo "${ttk} Installing ZED SDK v${ZED_SDK_MAJOR}.${ZED_SDK_MINOR} for Jetpack ${JETPACK_MAJOR}.${JETPACK_MINOR} (L4T v${L4T_MAJOR}.${L4T_MINOR})"
-sudo apt-get update -y || true
-sudo apt-get install -y --no-install-recommends wget less cmake curl gnupg2 \
+apt-get update -y || true
+apt-get install -y --no-install-recommends wget less cmake curl gnupg2 \
     build-essential python3 python3-pip python3-dev python3-setuptools libusb-1.0-0-dev -y && \
-    sudo -H pip install protobuf && \
+    pip install protobuf && \
     wget -q --no-check-certificate -O ZED_SDK_Linux_JP.run \
     https://download.stereolabs.com/zedsdk/${ZED_SDK_MAJOR}.${ZED_SDK_MINOR}/l4t${L4T_MAJOR}.${L4T_MINOR}/jetsons && \
     chmod +x ZED_SDK_Linux_JP.run ; ./ZED_SDK_Linux_JP.run silent skip_tools && \
     rm -rf /usr/local/zed/resources/* && \
     rm -rf ZED_SDK_Linux_JP.run && \
-    sudo rm -rf /var/lib/apt/lists/*
+    rm -rf /var/lib/apt/lists/*

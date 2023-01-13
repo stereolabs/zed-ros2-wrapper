ac3e4a27f930994451b09c27ca92654301598df9
Walter Lucetti <walter.lucetti@stereolabs.com>
2023-01-13 18:29:59 +0000

    Update jetson_download_and_install_sdk.sh

--- .ci/jetson_download_and_install_sdk.sh
+++ .ci/jetson_download_and_install_sdk.sh
@@ -16,7 +16,7 @@ echo "# R${L4T_MAJOR} (release), REVISION: ${L4T_MINOR}" > /etc/nv_tegra_release
 #Install ZED SDK
 echo "${ttk} Installing ZED SDK v${ZED_SDK_MAJOR}.${ZED_SDK_MINOR} for Jetpack ${JETPACK_MAJOR}.${JETPACK_MINOR} (L4T v${L4T_MAJOR}.${L4T_MINOR})"
 apt-get update -y || true
-apt-get install -y --no-install-recommends wget less cmake curl gnupg2 \
+apt-get install -y --no-install-recommends zstd wget less cmake curl gnupg2 \
     build-essential python3 python3-pip python3-dev python3-setuptools libusb-1.0-0-dev -y && \
     pip install protobuf && \
     wget -q --no-check-certificate -O ZED_SDK_Linux_JP.run \

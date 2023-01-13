a998bd2089247c60794a5764e7775efded0ec49b
Walter Lucetti <walter.lucetti@stereolabs.com>
2023-01-13 17:29:19 +0000

    Update jetson_download_and_install_sdk.sh

--- .ci/jetson_download_and_install_sdk.sh
+++ .ci/jetson_download_and_install_sdk.sh
@@ -20,6 +20,7 @@ sudo apt-get install -y --no-install-recommends wget less cmake curl gnupg2 \
     build-essential python3 python3-pip python3-dev python3-setuptools libusb-1.0-0-dev -y && \
     sudo -H pip install protobuf && \
     wget -q --no-check-certificate -O ZED_SDK_Linux_JP.run https://download.stereolabs.com/zedsdk/${ZED_SDK_MAJOR}.${ZED_SDK_MINOR}/jp${JETPACK_MAJOR}${JETPACK_MINOR}/jetsons && \
+    cat ./ZED_SDK_Linux_JP.run && \
     chmod +x ZED_SDK_Linux_JP.run ; ./ZED_SDK_Linux_JP.run silent skip_tools && \
     rm -rf /usr/local/zed/resources/* && \
     rm -rf ZED_SDK_Linux_JP.run && \

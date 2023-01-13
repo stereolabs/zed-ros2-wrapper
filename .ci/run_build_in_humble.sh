dc5ed591ebed252ea0f6cf0cbc58df6a3a4cbee5
Walter Lucetti <walter.lucetti@stereolabs.com>
2023-01-13 17:06:43 +0000

    Update run_build_in_humble.sh

--- .ci/run_build_in_humble.sh
+++ .ci/run_build_in_humble.sh
@@ -55,9 +55,10 @@ elif [[ $arch == "aarch64" ]]; then
 if [[ $ver == "20.04" ]]; then 
         JP_MAJOR=5
         JP_MINOR=0
+        L4T_MAJOR=35
         L4T_MINOR=1.0
         echo "${ttk} Install the ZED SDK"
-        . .ci/jetson_download_and_install_sdk.sh ${JP_MAJOR} ${ZED_SDK_MINOR} ${L4T_MINOR} ${ZED_SDK_MAJOR} ${ZED_SDK_MINOR}
+        . .ci/jetson_download_and_install_sdk.sh ${JP_MAJOR} ${ZED_SDK_MINOR} ${L4T_MAJOR} ${L4T_MINOR} ${ZED_SDK_MAJOR} ${ZED_SDK_MINOR}
         echo "${ttk} Build ROS2 Humble from the source."    
         #. .ci/jetson_build_humble_src.sh    
     fi

605e59c6a65bca0e4d4fadee0ffa77a1b35f1031
Walter Lucetti <walter.lucetti@stereolabs.com>
2023-01-16 13:24:42 +0000

    Update jetson_build_humble_src.sh

--- .ci/jetson_build_humble_src.sh
+++ .ci/jetson_build_humble_src.sh
@@ -25,12 +25,7 @@ echo "${ttk} ROS2 Workspace: ${WS_DIR}"
 echo "${ttk} Check environment variables"
 env | grep ROS
 
-echo "${ttk} Update bin repositories"
-apt-get update || true
-#TZ="Europe/Paris" apt-get upgrade --yes
-rosdep update
-
-echo "${ttk} Install ZED ROS2 Package dependencies from the sources"
+echo "${ttk} Install missing ZED ROS2 Package dependencies from the sources"
 cd ${WS_DIR}/src
 XACRO_VERSION=2.0.8
 wget https://github.com/ros/xacro/archive/refs/tags/${XACRO_VERSION}.tar.gz -O - | tar -xvz && mv xacro-${XACRO_VERSION} xacro
@@ -39,27 +34,17 @@ wget https://github.com/ros/diagnostics/archive/refs/tags/${DIAGNOSTICS_VERSION}
 AMENT_LINT_VERSION=0.12.4
 wget https://github.com/ament/ament_lint/archive/refs/tags/${AMENT_LINT_VERSION}.tar.gz -O - | tar -xvz && mv ament_lint-${AMENT_LINT_VERSION} ament-lint
 
-#echo "${ttk} Current folder"
-#pwd
-#echo "${ttk} Content"
-#ls -lah
-
-echo "${ttk} Build the dependencies"
-cd ${WS_DIR}
-rosdep install --from-paths src --ignore-src -r -y
-colcon build --cmake-args=-DCMAKE_BUILD_TYPE=Release --parallel-workers $(nproc)
-
 echo "${ttk} Copy the ZED ROS2 Package sources in the workspace"
 cd ${WORKDIR}
 cd ..
 echo "cp -a ./${PROJ_NAME} ${WS_DIR}/src/"
 cp -a ./${PROJ_NAME} ${WS_DIR}/src/
 
-echo "${ttk} Check that all the dependencied of the ZED ROS2 Package are satisfied"
+echo "${ttk} Check that all the dependencies are satisfied"
 cd ${WS_DIR}
 rosdep install --from-paths src --ignore-src -r -y
 
-echo "${ttk} Build the ZED ROS2 Package"
+echo "${ttk} Build the ZED ROS2 Package and the dependencies"
 cd ${WS_DIR}
 colcon build --cmake-args=-DCMAKE_BUILD_TYPE=Release --parallel-workers $(nproc)
 

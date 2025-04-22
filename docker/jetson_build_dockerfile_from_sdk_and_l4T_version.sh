508ea99e5eea96fe51764546dd17df8d4534b703
Aymeric DUJARDIN <aymeric.dujardin@gmail.com>
2025-04-22 14:02:29 +0200

    Adding Override SDK URL option dockerfiles

--- docker/jetson_build_dockerfile_from_sdk_and_l4T_version.sh
+++ docker/jetson_build_dockerfile_from_sdk_and_l4T_version.sh
@@ -18,15 +18,9 @@ L4T_version=$1
 # Remove the prefix 'l4t-r'
 l4t_version_number="${L4T_version#l4t-r}"
 
-# Verify the ZED SDK format (digits.digits.digits)
-if ! [[ $2 =~ ^zedsdk-[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    echo "Invalid ZED SDK version format."
-    exit 1
-fi
 
 ZED_SDK_version=$2
-# Remove the prefix 'zedsdk-'
-zed_sdk_version_number="${ZED_SDK_version#zedsdk-}"
+
 
 # copy the wrapper content
 rm -r ./tmp_sources
@@ -37,9 +31,6 @@ cp -r ../zed* ./tmp_sources
 IFS='.' read -r l4t_major l4t_minor l4t_patch <<< "$l4t_version_number"
 ###########
 
-# Split the string and assign to variables
-IFS='.' read -r sdk_major sdk_minor sdk_patch <<< "$zed_sdk_version_number"
-
 L4T_VERSION=${1:-l4t-r${l4t_major}.${l4t_minor}.${l4t_patch}}
 
 # Determine the IMAGE_NAME based on the L4T_VERSION
@@ -49,16 +40,46 @@ else
     IMAGE_NAME="dustynv/ros:humble-ros-base-${L4T_VERSION}"
 fi
 
-echo "Building dockerfile for $1 and ZED SDK $2"
-docker build -t zed_ros2_l4t_${l4t_major}.${l4t_minor}.${l4t_patch}_sdk_${sdk_major}.${sdk_minor}.${sdk_patch} \
---build-arg ZED_SDK_MAJOR=$sdk_major \
---build-arg ZED_SDK_MINOR=$sdk_minor \
---build-arg ZED_SDK_PATCH=$sdk_patch \
---build-arg L4T_VERSION=$1 \
---build-arg L4T_MAJOR=$l4t_major \
---build-arg L4T_MINOR=$l4t_minor \
---build-arg IMAGE_NAME=$IMAGE_NAME \
--f ./Dockerfile.l4t-humble .
+# Check if the third arg is a custom path
+CUSTOM_ZED_SDK_URL=$2
+# Use curl to check if the URL is valid (returns HTTP 200)
+if [ "$(curl -L -I "${CUSTOM_ZED_SDK_URL}" -o /dev/null -s -w '%{http_code}\n' | head -n 1)" = "200" ]; then
+    echo "${ZED_SDK_version} detected as a valid custom installer URL"
+
+    echo "Building dockerfile for $1 and a custom ZED SDK"
+
+    docker build -t zed_ros2_l4t_${l4t_major}.${l4t_minor}.${l4t_patch}_sdk_custom \
+    --build-arg CUSTOM_ZED_SDK_URL=$CUSTOM_ZED_SDK_URL \
+    --build-arg L4T_VERSION=$1 \
+    --build-arg L4T_MAJOR=$l4t_major \
+    --build-arg L4T_MINOR=$l4t_minor \
+    --build-arg IMAGE_NAME=$IMAGE_NAME \
+    -f ./Dockerfile.l4t-humble .
+else
+    # Verify the ZED SDK format (digits.digits.digits)
+    if ! [[ $2 =~ ^zedsdk-[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        echo "Invalid ZED SDK version format."
+        exit 1
+    fi
+
+    # Remove the prefix 'zedsdk-'
+    zed_sdk_version_number="${ZED_SDK_version#zedsdk-}"
+
+    # Split the string and assign to variables
+    IFS='.' read -r sdk_major sdk_minor sdk_patch <<< "$zed_sdk_version_number"
+
+    echo "Building dockerfile for $1 and ZED SDK $2"
+
+    docker build -t zed_ros2_l4t_${l4t_major}.${l4t_minor}.${l4t_patch}_sdk_${sdk_major}.${sdk_minor}.${sdk_patch} \
+    --build-arg ZED_SDK_MAJOR=$sdk_major \
+    --build-arg ZED_SDK_MINOR=$sdk_minor \
+    --build-arg ZED_SDK_PATCH=$sdk_patch \
+    --build-arg L4T_VERSION=$1 \
+    --build-arg L4T_MAJOR=$l4t_major \
+    --build-arg L4T_MINOR=$l4t_minor \
+    --build-arg IMAGE_NAME=$IMAGE_NAME \
+    -f ./Dockerfile.l4t-humble .
+fi
 
 # Remove the temporary folder
 rm -r ./tmp_sources
